e1b9679e9aa7c043b1e35af01bf13e3176604cf8
wxy <1159530410@qq.com>
2020-01-12 13:35:12 +0800

    one

--- SpringCloud/SpringCloudLab1/provider/src/main/java/com/sise/provider/controller/FirstController.java
+++ SpringCloud/SpringCloudLab1/provider/src/main/java/com/sise/provider/controller/FirstController.java
@@ -1,9 +1,15 @@
 package com.sise.provider.controller;
 
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.google.common.collect.Maps;
+import com.sise.provider.entity.Person;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * @program: SpringCloudLab1
@@ -15,9 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class FirstController {
 
-    @RequestMapping(value = "/{name}", method = RequestMethod.GET)
-    public String findName(@PathVariable("name") String name){
-        return name;
+
+    /**
+     * @date: 2020/1/12
+     * @description: Lab1
+     */
+
+    @RequestMapping(value = "/{personId}", method = RequestMethod.GET)
+    public String findName(@PathVariable("personId") Integer personId){
+        Person person = new Person(personId, "TOM", 23);
+        Map<String, String> parameterMap = Maps.newHashMap();
+        parameterMap.put("PersonId", person.getPersonId().toString());
+        parameterMap.put("Pname", person.getPname());
+        parameterMap.put("Page", Integer.toString(person.getPage()));
+        String str = JSON.toJSONString(parameterMap);
+        return str;
     }
 
 }
