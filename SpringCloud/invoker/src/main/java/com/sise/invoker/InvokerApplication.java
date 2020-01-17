270295a7c4bfeea0f6730ee91ae89c44bf9e9baa
wxy <1159530410@qq.com>
2020-01-17 11:45:16 +0800

    Lab4

--- SpringCloud/invoker/src/main/java/com/sise/invoker/InvokerApplication.java
+++ SpringCloud/invoker/src/main/java/com/sise/invoker/InvokerApplication.java
@@ -1,14 +1,18 @@
 package com.sise.invoker;
 
+import com.netflix.config.ConfigurationManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class InvokerApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        ConfigurationManager.loadPropertiesFromResources("application.yml");
         SpringApplication.run(InvokerApplication.class, args);
     }
 
