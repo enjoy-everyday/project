f846f4ba7bcb41e701877a29161fb61d62dc6c5a
wxy <1159530410@qq.com>
2020-01-23 11:00:24 +0800

    Lab7

---
+++ SpringCloud/hystrixclient/src/test/java/com/sise/hystrixClient/HystrixClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sise.hystrixClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
