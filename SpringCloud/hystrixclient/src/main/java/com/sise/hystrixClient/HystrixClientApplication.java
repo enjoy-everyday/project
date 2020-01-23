4c861622f4277129930cc2797b8368dabde78f30
wxy <1159530410@qq.com>
2020-01-23 10:59:16 +0800

    Lab7

---
+++ SpringCloud/hystrixclient/src/main/java/com/sise/hystrixClient/HystrixClientApplication.java
@@ -0,0 +1,13 @@
+package com.sise.hystrixClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixClientApplication.class, args);
+    }
+
+}
