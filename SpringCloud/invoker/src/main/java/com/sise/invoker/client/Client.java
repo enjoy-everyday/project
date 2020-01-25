8fac9853f48414ce8402c7edeecb7a2ff304e191
wxy <1159530410@qq.com>
2020-01-25 15:40:25 +0800

    Lab9

--- SpringCloud/invoker/src/main/java/com/sise/invoker/client/Client.java
+++ SpringCloud/invoker/src/main/java/com/sise/invoker/client/Client.java
@@ -3,6 +3,7 @@ package com.sise.invoker.client;
 import com.sise.invoker.custom.MyUrl;
 import com.sise.invoker.entity.Person;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,19 +15,42 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * @create: 2020-01-20 17:10
  **/
 
-@FeignClient(name = "provider")
-public interface Client {
-
+//@FeignClient(name = "provider")
+//public interface Client {
+//
+////    @RequestMapping(method = RequestMethod.GET, value = "/hello")
+////    String hello();
+//
+//    @RequestMapping(method = RequestMethod.GET, value = "/{personId}")
+//    Person getPerson(@PathVariable("personId") Integer personId);
+//
+//    @MyUrl(method = "GET", url = "/hello")
+//    String custom();
+//
 //    @RequestMapping(method = RequestMethod.GET, value = "/hello")
-//    String hello();
+//    String customHello();
+//
+//}
 
-    @RequestMapping(method = RequestMethod.GET, value = "/{personId}")
-    Person getPerson(@PathVariable("personId") Integer personId);
+    /**
+     * @date: 2020/1/24
+     * @description: Lab9
+     */
 
-    @MyUrl(method = "GET", url = "/hello")
-    String custom();
+    @FeignClient(name = "provider", fallback = Client.ClientFallback.class)
+    public interface Client {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/hello")
-    String customHello();
+        @RequestMapping(method = RequestMethod.GET, value = "/hello")
+        public String hello();
 
-}
+        @Component
+        static class ClientFallback implements Client{
+
+            public String hello(){
+                return "error";
+            }
+
+        }
+
+    }
+
