c382b744956bdbb4322a238aad9362cb8c97521a
wxy <1159530410@qq.com>
2020-01-26 12:59:37 +0800

    Lab9

---
+++ SpringCloud/invoker/src/main/java/com/sise/invoker/exception/MyException.java
@@ -0,0 +1,11 @@
+package com.sise.invoker.exception;
+
+/**
+ * @program: SpringCloud
+ * @description: Lab9
+ * @author: wxy
+ * @create: 2020-01-24 15:19
+ **/
+
+public class MyException extends Throwable {
+}
