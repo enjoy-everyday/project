6357ebc7bbf1c366a484b65a18964946d0277c50
wxy <1159530410@qq.com>
2020-01-26 13:00:30 +0800

    Lab9

--- SpringCloud/invoker/src/main/java/com/sise/invoker/InvokerApplication.java
+++ SpringCloud/invoker/src/main/java/com/sise/invoker/InvokerApplication.java
@@ -3,14 +3,21 @@ package com.sise.invoker;
 import com.netflix.config.ConfigurationManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
+@ServletComponentScan
 public class InvokerApplication {
 
     /**
@@ -27,6 +34,21 @@ public class InvokerApplication {
      * @date: 2020/1/20
      * @description: Lab6
      */
+
+//    public static void main(String[] args) {
+//        SpringApplication.run(InvokerApplication.class, args);
+//    }
+
+    /**
+     * @date: 2020/1/24
+     * @description: Lab9
+     */
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate getRestTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(InvokerApplication.class, args);
