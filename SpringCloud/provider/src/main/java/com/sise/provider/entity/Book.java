3a76e00909211563eb3ffc3df2cde354b9f80a4e
wxy <1159530410@qq.com>
2020-01-27 11:11:32 +0800

    Lab10

---
+++ SpringCloud/provider/src/main/java/com/sise/provider/entity/Book.java
@@ -0,0 +1,40 @@
+package com.sise.provider.entity;
+
+/**
+ * @program: SpringCloud
+ * @description: Lab10
+ * @author: wxy
+ * @create: 2020-01-26 14:33
+ **/
+
+public class Book {
+
+    private Integer id;
+    private String name;
+    private String author;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+}
