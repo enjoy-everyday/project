eb2732a44a1b69d01e536f01548f75d03ecb875c
wxy <1159530410@qq.com>
2020-01-27 11:09:57 +0800

    Lab10

---
+++ SpringCloud/zuulgateway/src/main/java/com/sise/zuulGateway/controller/SourceController.java
@@ -0,0 +1,23 @@
+package com.sise.zuulGateway.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: SpringCloud
+ * @description: Lab10
+ * @author: wxy
+ * @create: 2020-01-26 16:38
+ **/
+
+@RestController
+public class SourceController {
+
+    @RequestMapping(value = "/source/{name}", method = RequestMethod.GET)
+    public String hello(@PathVariable("name") String name){
+        return "Hello" + name;
+    }
+
+}
