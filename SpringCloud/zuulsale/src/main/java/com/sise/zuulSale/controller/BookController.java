66a22a26dbeabce422a1b2228dcdfd659f774bfb
wxy <1159530410@qq.com>
2020-01-28 11:27:35 +0800

    Lab10

--- SpringCloud/zuulsale/src/main/java/com/sise/zuulSale/controller/BookController.java
+++ SpringCloud/zuulsale/src/main/java/com/sise/zuulSale/controller/BookController.java
@@ -1,7 +1,9 @@
 package com.sise.zuulSale.controller;
 
 import com.sise.zuulSale.entity.Book;
+import com.sise.zuulSale.entity.Person;
 import com.sise.zuulSale.service.BookService;
+import com.sise.zuulSale.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,12 +25,28 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @RequestMapping(value = "saleBook/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "book-server/{id}", method = RequestMethod.GET)
     public String saleBook(@PathVariable Integer id){
         Book book = bookService.getBook(id);        //调用Book服务查看
         System.out.println("id：" + book.getId());       //控制台输出，模拟销售
         System.out.println("书名：" + book.getName());
         return "SUCCESS";
+    }
+
+    /**
+     * @date: 2020/1/27
+     * @description: Lab10 实验
+     */
+
+    @Autowired
+    private PersonService personService;
+
+    @RequestMapping(value = "person-server/{id}", method = RequestMethod.GET)
+    public String salePerson(@PathVariable Integer id){
+        Person person = personService.getPerson(id);
+        System.out.println("id：" + person.getId());
+        System.out.println("姓名：" + person.getName());
+        return "ok";
     }
 
 }
