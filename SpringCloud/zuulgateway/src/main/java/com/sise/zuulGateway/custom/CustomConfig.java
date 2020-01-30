04f6450b3208c1e188d0271eac59158e7c24decb
wxy <1159530410@qq.com>
2020-01-30 11:22:35 +0800

    Lab11

--- SpringCloud/zuulgateway/src/main/java/com/sise/zuulGateway/custom/CustomConfig.java
+++ SpringCloud/zuulgateway/src/main/java/com/sise/zuulGateway/custom/CustomConfig.java
@@ -1,22 +1,22 @@
-package com.sise.zuulGateway.custom;
-
-import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * @program: SpringCloud
- * @description: Lab11
- * @author: wxy
- * @create: 2020-01-28 11:31
- **/
-
-@Configuration
-public class CustomConfig {
-
-    @Bean
-    public PatternServiceRouteMapper patternServiceRouteMapper(){
-        return new PatternServiceRouteMapper("(?<module>.+)-(service)", "${module}/**");
-    }
-
-}
+//package com.sise.zuulGateway.custom;
+//
+//import org.springframework.cloud.netflix.zuul.filters.discovery.PatternServiceRouteMapper;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @program: SpringCloud
+// * @description: Lab11
+// * @author: wxy
+// * @create: 2020-01-28 11:31
+// **/
+//
+//@Configuration
+//public class CustomConfig {
+//
+//    @Bean
+//    public PatternServiceRouteMapper patternServiceRouteMapper(){
+//        return new PatternServiceRouteMapper("(?<module>.+)-(service)", "${module}/**");
+//    }
+//
+//}
