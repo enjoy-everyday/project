2e67b383fb749cdf9f031d38ad93e2da616e5967
wxy <1159530410@qq.com>
2020-02-01 11:57:36 +0800

    综合实验

--- SpringCloud/rabbitprovider/src/main/java/com/sise/rabbitProvider/RabbitProviderApplication.java
+++ SpringCloud/rabbitprovider/src/main/java/com/sise/rabbitProvider/RabbitProviderApplication.java
@@ -4,11 +4,13 @@ import com.sise.rabbitProvider.service.SendService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableBinding(SendService.class)       //容器启动时，会自动绑定SendService接口中定义的通道
+@EnableFeignClients
 public class RabbitProviderApplication {
 
     public static void main(String[] args) {
