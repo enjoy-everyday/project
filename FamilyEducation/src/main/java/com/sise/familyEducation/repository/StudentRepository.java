e2a8c2b03ad01b2ee38d9719d5025195edb72cf6
wxy <1159530410@qq.com>
2020-03-25 15:32:32 +0800

    repository

--- FamilyEducation/src/main/java/com/sise/familyEducation/repository/StudentRepository.java
+++ FamilyEducation/src/main/java/com/sise/familyEducation/repository/StudentRepository.java
@@ -22,4 +22,13 @@ public interface StudentRepository extends JpaRepository<Student, Integer> {
 
     Student findByUser(User user);
 
+    @Query(nativeQuery = true, value = "SELECT * FROM student WHERE qualification != ?1 AND score >= ?2")
+    List<Student> findStudentsByQualificationNotAndScore(String qualification, float score);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM student WHERE qualification LIKE '%士%' AND score >= ?1")
+    List<Student> findStudentsByQualificationLikeAndScore(float score);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM student WHERE qualification =?1 AND score >= ?2")
+    List<Student> findStudentsByQualificationAndScore(String qualification, float score);
+
 }
